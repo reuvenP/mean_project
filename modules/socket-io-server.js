debac4ae6fbe98e11fa079e2ff7f422f48ce08cc
reuvenp <reuvenp@checkpoint.com>
2017-04-02 23:53:17 +0300

    io started

--- modules/socket-io-server.js
+++ modules/socket-io-server.js
@@ -5,8 +5,12 @@
 var dal_messages = require('./dal_messages');
 var dal_rooms = require('./dal_rooms');
 var dat_users = require('./dal_users');
-var io = require('socket.io')(http);
 
-io.on('connection', function (socket) {
+var configSocketIo = function(httpServer) {
+    var io = require('socket.io')(httpServer);
+    io.on('connection', function (socket) {
 
-});
+    });
+};
+
+module.exports = configSocketIo;
