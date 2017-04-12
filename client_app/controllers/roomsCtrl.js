086683e81a1b54c7109fe1ae2c61946230c7339e
reuvenp <reuvenp@checkpoint.com>
2017-04-13 02:51:39 +0300

    rooms

--- client_app/controllers/roomsCtrl.js
+++ client_app/controllers/roomsCtrl.js
@@ -1,11 +1,13 @@
 /**
  * Created by reuvenp on 4/13/2017.
  */
-angular.module('myApp').controller('roomsController', ['$scope', 'pageService', 'roomsService', 'usersService', roomsController]);
-function roomsController($scope, pageService, roomsService, usersService) {
+angular.module('myApp').controller('roomsController', ['$scope', 'pageService', 'roomsService', 'usersService', 'chatService', roomsController]);
+function roomsController($scope, pageService, roomsService, usersService, chatService) {
     var vm = this;
     vm.mainData = pageService.mainData;
     vm.searchFilter = {};
+    vm.usersList = usersService.usersList;
+
 
     pageService.setPageTitle('Rooms Management');
 
