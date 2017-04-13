91bc5d7f01893c6299f72c7ae3a302ef884992d2
reuvenp <reuvenp@checkpoint.com>
2017-04-14 00:31:54 +0300

    2 more cameras

--- routes/webcamRoute.js
+++ routes/webcamRoute.js
@@ -15,7 +15,8 @@ router.get('/webcamsURL', function(req, res, next) {
         'http://75.151.65.9/mjpg/video.mjpg?COUNTER',
         'http://46.146.210.29/mjpg/video.mjpg?COUNTER',
         'http://41.160.227.150:8083/mjpg/video.mjpg?COUNTER',
-        'http://88.190.98.55/mjpg/video.mjpg?COUNTER'
+        'http://88.190.98.55/mjpg/video.mjpg?COUNTER',
+        'http://220.240.123.205/mjpg/video.mjpg?COUNTER'
     ];
 
     res.json(webcams);
