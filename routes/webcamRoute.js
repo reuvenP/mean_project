fccdceca1a344b2ee4f89ef77d2c6bbb721726d3
reuvenp <reuvenp@checkpoint.com>
2017-04-16 00:11:20 +0300

    Headers in  webcams

--- routes/webcamRoute.js
+++ routes/webcamRoute.js
@@ -11,12 +11,13 @@ router.get('/webcamsURL', function(req, res, next) {
     }
 
     var webcams = [
-        'http://69.193.149.90:82/mjpg/video.mjpg?COUNTER',
-        'http://75.151.65.9/mjpg/video.mjpg?COUNTER',
-        'http://46.146.210.29/mjpg/video.mjpg?COUNTER',
-        'http://41.160.227.150:8083/mjpg/video.mjpg?COUNTER',
-        'http://88.190.98.55/mjpg/video.mjpg?COUNTER',
-        'http://220.240.123.205/mjpg/video.mjpg?COUNTER'
+        {header: 'Elevator', url: 'http://69.193.149.90:82/mjpg/video.mjpg?COUNTER'},
+        {header: 'Pool', url: 'http://75.151.65.9/mjpg/video.mjpg?COUNTER'},
+        {header: 'Lobby', url: 'http://46.146.210.29/mjpg/video.mjpg?COUNTER'},
+        {header: 'Control', url: 'http://41.160.227.150:8083/mjpg/video.mjpg?COUNTER'},
+        {header: 'Buffet', url: 'http://88.190.98.55/mjpg/video.mjpg?COUNTER'},
+        {header: 'Avenue', url: 'http://220.240.123.205/mjpg/video.mjpg?COUNTER'},
+        {header: 'Bar', url: 'http://83.40.74.116:81/mjpg/video.mjpg?COUNTER'}
     ];
 
     res.json(webcams);
