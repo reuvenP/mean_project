2613ea0dbbb70fece821d33cff914f0cb04b0eef
Ariel Ben-Ami <aba4000@gmail.com>
2017-04-16 05:17:29 +0300

    search page

---
+++ client_app/controllers/searchCtrl.js
@@ -0,0 +1,79 @@
+angular.module('myApp').controller('searchCtrl', ['pageService', 'chatService', 'usersService', 'roomsService', searchCtrl]);
+function searchCtrl(pageService, chatService, usersService) {
+    var vm = this;
+    vm.mainData = pageService.mainData;
+    vm.usersList = usersService.usersList;
+    vm.rooms = chatService.rooms;
+    vm.messages = [];
+    vm.formatDateTime = pageService.formatDateTime;
+    vm.searchFilter = {};
+
+    vm.selectedRoomChanged = function() {
+        chatService.getAllMessages(vm.selectedRoom).then(
+            function() {
+                vm.messages = chatService.getRoom(vm.selectedRoom).messages;
+                pageService.clearAlert();
+            },
+            function (res) {
+                pageService.showResponseError(res);
+            }
+        )
+    };
+
+    vm.senderImage = function(senderId) {
+        var image = usersService.getUserById(senderId).image;
+        return image || 'public/img/user_avatar.png';
+    };
+
+    vm.senderName = function(senderId) {
+        return usersService.getUserById(senderId).name;
+    };
+
+    vm.dateRangeFilter = function(value, index, array) {
+        if (!vm.fromDate && !vm.toDate) {
+            return true;
+        }
+
+        var testDate = new Date(value.submitDate);
+        if (vm.fromDate) {
+            var fromDate = new Date(vm.fromDate);
+            if (!vm.toDate) {
+                return testDate >= fromDate;
+            }
+            else {
+                var toDate = new Date(vm.toDate);
+                toDate.setDate(toDate.getDate() + 1);
+                return testDate >= fromDate && testDate < toDate;
+            }
+        }
+        else {
+            var toDate = new Date(vm.toDate);
+            toDate.setDate(toDate.getDate() + 1);
+            return testDate < toDate;
+        }
+    };
+
+    pageService.setPageTitle('Rooms Search', 'Find messages inside rooms');
+
+    $(document).ready(function() {
+        $('#fromDatepicker, #toDatepicker').datepicker({
+            autoclose: true,
+            format: 'dd MM yyyy'
+        });
+    });
+
+    usersService.refreshUsers().then(
+        function() {
+            chatService.getMyRooms().then(
+                function(res) {
+                    pageService.clearAlert();
+                },
+                function (res) {
+                    pageService.showResponseError(res);
+                }
+            )
+        }, function (res) {
+            pageService.showResponseError(res);
+        }
+    );
+}
