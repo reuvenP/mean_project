8259a148113850af3506b99947dba3eb8406d569
Ariel Ben-Ami <aba4000@gmail.com>
2017-04-18 23:33:31 +0300

    set the active side links

--- client_app/controllers/mainCtrl.js
+++ client_app/controllers/mainCtrl.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('mainCtrl', ['$scope', 'pageService', 'loginService', mainCtrl]);
-function mainCtrl($scope, pageService, loginService) {
+angular.module('myApp').controller('mainCtrl', ['$scope', 'pageService', 'loginService', 'usersService', mainCtrl]);
+function mainCtrl($scope, pageService, loginService, usersService) {
     var vm = this;
     vm.mainData = pageService.mainData;
 
@@ -13,6 +13,23 @@ function mainCtrl($scope, pageService, loginService) {
                 window.location.href  = "/";
             }
         )
+    };
+
+    vm.editMyUser = function () {
+        var user = vm.mainData.myUser;
+        if (!user) return;
+        pageService.clearAlert();
+        var modal = usersService.openUserEditModal(angular.copy(user));
+        modal.then(function(userResult) {
+            usersService.editUser(userResult).then(
+                function (res) {
+                    pageService.refreshMyUserIfNeeded(userResult);
+                    pageService.showAlert('User updated successfully', 'success', 'Success');
+                }, function (res) {
+                    pageService.showAlert(res.status + ' - ' + res.statusText + ": " + (res.data.message || res.data.errmsg || res.data), 'danger', 'Error');
+                }
+            )
+        });
     };
 
     $scope.$on('$locationChangeStart', function(event) {
